a72ac570e9e59474a0848a03cd2789b5dc049f90
Caio Vieira <caiovieiraprofissional@protonmail.com>
2021-07-23 13:17:35 -0300

    updated crate version and switched xor development to a separate branch

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -93,46 +93,6 @@ pub fn b64(vw: &str) -> CipherResult {
 }
 
 pub fn xor(uw: &str, vw: &str) -> CipherResult {
-
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let mut binary_vw_word = "".to_string();
-    let mut binary_uw_word = "".to_string();
-    let mut new_pass = String::new();
-
-    for i in vw.clone().bytes() {
-        binary_vw_word += &format!("0{:b}", i);
-    }
-    for i in uw.clone().bytes() {
-        binary_uw_word += &format!("0{:b}", i);
-    }
-
-    println!("{:?}", binary_uw_word);
-    println!("{:?}", binary_vw_word);
-
-    let mut x = 0;
-    let mut binary_pass = String::new();
-    for i in 0..uw.len() {
-        if x == 8 {binary_pass += " "; x = 0}
-        let uw_val = &(binary_uw_word.as_bytes()[{
-                            if i > binary_uw_word.len() - 1 {
-                                i - (binary_uw_word.len() * (i/binary_uw_word.len() as usize))
-                            } else {
-                                i
-                            }
-                        }] as char).to_string().parse::<u8>().unwrap();
-        let vw_val = &(binary_vw_word.as_bytes()[i] as char).to_string().parse().unwrap();
-        binary_pass += &format!("0{:b}", uw_val^vw_val);
-        x += 1;
-    }
-    
-    let binary_vec: Vec<&str> = binary_pass.split(" ").collect();
-    for i in binary_vec{
-        let number = usize::from_str_radix(i, 2).unwrap();
-        new_pass += &(alphabet.as_bytes()[number] as char).to_string();
-    }
-
-    println!("{:?}", binary_pass);
-
     Ok(String::new())
 }
 
